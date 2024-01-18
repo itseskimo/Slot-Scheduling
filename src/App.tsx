2c70eb7be10933baef8481bf9a7e9184173d5b3f
itseskimo <ayushmatmotgill@gmail.com>
2024-01-18 08:49:33 +0530

    update: optimizing form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,59 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import { formSvgData } from './config/data'
+import React, { useState, ChangeEvent } from 'react';
+
+
+interface FormInputProps {
+  svgData: string;
+  placeholder: string;
+  type: string;
+  value: string | number;
+  onChange: (value: string) => void;
+}
+
+const FormInput: React.FC<FormInputProps> = ({ svgData, placeholder, type, value, onChange }) => {
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  return (
+    <div className='flex items-center relative w-full '>
+      <span className='absolute left-0 px-2' dangerouslySetInnerHTML={{ __html: svgData }}></span>
+      <input
+        className='border-solid border-[1px] border-[#AFB9BB] outline-none rounded-[6px] py-[6px] text-[12px] md:text-[16px] md:py-[10px] pl-10 pr-5 w-full text-[#4C5864]  placeholder:text-[#4C5864]'
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+
+
+
+
 function App() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    age: '',
+    city:'',
+    company:''
+    // Add more fields as needed
+  });
+  
+  console.log(formData)
+  const handleInputChange = (fieldName: string, value: string) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
 
   return (
     <>
@@ -14,33 +66,13 @@ function App() {
             <h6 className='font-bold text-[26px] text-center ' >Book an Appointment for <br></br> <span className='line-through text-[#00acc1] decoration-white'>Rs 1000</span>  FREE</h6>
             <p className='text-center text-[#00acc1]'>60+ Expert Physiotherapists for 200+ Conditions</p>
 
-            <div className='flex items-center relative w-full '>
-              <span className="absolute left-0 px-2" dangerouslySetInnerHTML={{ __html: formSvgData[0] }}></span>
-              <input className='border-solid border-[1px] border-[#AFB9BB] outline-none rounded-[6px] py-[6px] text-[12px] md:text-[16px] md:py-[10px] pl-10 pr-5 w-full text-[#4C5864]  placeholder:text-[#4C5864]' placeholder='Your Name' type="email" />
-            </div>
-
-            <div className='flex items-center relative w-full '>
-              <span className="absolute left-0 px-2" dangerouslySetInnerHTML={{ __html: formSvgData[1] }}></span>
-              <input className='border-solid border-[1px] border-[#AFB9BB] outline-none rounded-[6px] py-[6px] text-[12px] md:text-[16px] md:py-[10px] pl-10 pr-5 w-full text-[#4C5864]  placeholder:text-[#4C5864]' placeholder='+91' type="number" />
-            </div>
+            <FormInput svgData={formSvgData[0]} placeholder='Your Name' type='text' value={formData.name} onChange={(value) => handleInputChange('name', value)} />
+            <FormInput svgData={formSvgData[1]} placeholder='+91' type='number' value={formData.phone} onChange={(value) => handleInputChange('phone', value)} />
 
             <section className='flex items-center gap-4'>
-              
-              <div className='flex items-center relative w-full '>
-                <span className="absolute left-0 px-2" dangerouslySetInnerHTML={{ __html: formSvgData[2] }}></span>
-                <input className='border-solid border-[1px] border-[#AFB9BB] outline-none rounded-[6px] py-[6px] text-[12px] md:text-[16px] md:py-[10px] pl-10 pr-3 w-full text-[#4C5864]  placeholder:text-[#4C5864]' placeholder='Your Age' type="email" />
-              </div>
-
-              <div className='flex items-center relative w-full '>
-                <span className="absolute left-0 px-2" dangerouslySetInnerHTML={{ __html: formSvgData[3] }}></span>
-                <input className='border-solid border-[1px] border-[#AFB9BB] outline-none rounded-[6px] py-[6px] text-[12px] md:text-[16px] md:py-[10px] pl-10 pr-3 w-full text-[#4C5864]  placeholder:text-[#4C5864]' placeholder='Your City' type="email" />
-              </div>
-
-              <div className='flex items-center relative w-full '>
-                <span className="absolute left-0 px-2" dangerouslySetInnerHTML={{ __html: formSvgData[4] }}></span>
-                <input className='border-solid border-[1px] border-[#AFB9BB] outline-none rounded-[6px] py-[6px] text-[12px] md:text-[16px] md:py-[10px] pl-10 pr-3 w-full text-[#4C5864]  placeholder:text-[#4C5864]' placeholder='Your Company' type="email" />
-              </div>
-
+            <FormInput svgData={formSvgData[3]} placeholder='Your Age' type='number' value={formData.age} onChange={(value) => handleInputChange('age', value)} />
+            <FormInput svgData={formSvgData[4]} placeholder='Your City' type='text' value={formData.city} onChange={(value) => handleInputChange('city', value)} />
+            <FormInput svgData={formSvgData[5]} placeholder='Your Company' type='text' value={formData.company} onChange={(value) => handleInputChange('company', value)} />
             </section>
 
 
