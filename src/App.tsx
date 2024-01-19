f3e8e4592644122bd2b2b18ea7d764047252cd1e
itseskimo <ayushmatmotgill@gmail.com>
2024-01-19 10:13:16 +0530

    update: redesigining the layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Navbar />
 
-      <main className='grid grid-cols-2 bg-[#060f17] relative'>
+      <main className='grid grid-cols-2 bg-[#060f17] relative' >
         {/* Overlay */}
         <div style={{ background: ' linear-gradient(180deg, rgba(255, 255, 255, 0) 72.21%, #060f17 95.42%)' }} className='absolute w-full h-full'> </div>
         <BookingForm />
